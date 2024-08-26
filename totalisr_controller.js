978fe4f296272ed3e76a09a8941c77fc419a92a9
Stuart Leitch <stuart.leitch@leitches.net>
2024-08-26 14:59:57 +0100

    Add buttons to calculate and add an item

--- totalisr_controller.js
+++ totalisr_controller.js
@@ -5,10 +5,32 @@ export default class extends Controller {
   static values = { total: Number }
 
   connect() {
+    this.addItemI("Budget", 1000)
+    this.addItemI("Flights", -250)
+    this.addItemI("Accomodation", -500)
+    this.calculate()
+  }
+
+  addItemI(itemName, itemValue) {
+    var tb = this.element.tBodies[0]
+    var newRow = tb.insertRow(-1)
+    var newCell = newRow.insertCell(0)
+    newCell.textContent = itemName
+    newCell = newRow.insertCell(1)
+    newCell.textContent = itemValue
+    newCell.setAttribute("data-totalisr-target", "item")
+  }
+
+  addItem({ params: { itemName, itemValue } }) {
+    console.log(itemName, itemValue)
+    this.addItemI(itemName, itemValue)
+  }
+
+  calculate() {
+    this.totalValue = 0
     this.itemTargets.forEach((item, index) => {
       this.totalValue += Number(item.textContent)
     })
     this.totalTarget.textContent = `${this.totalValue}`
   }
-
 }
